9d673bd142a3dd87854ec92d1c8dfa881a61a824
Giuseppe Formisano <giuseppe.a.formisano@accenture.com>
2026-08-28 14:41:31 +0200

    chore: bump versione 0.13.10-beta (versionCode 223)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.music.livefeed"
     minSdk = 24
     targetSdk = 36
-    versionCode = 222
-    versionName = "0.13.9-beta"
+    versionCode = 223
+    versionName = "0.13.10-beta"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
